ae9a320356c809ed355bc45cb1da3612f23ee6c3
NamTong1998 <nam.th.vnist@gmail.com>
2019-05-22 01:02:06 +0700

    last codes:

--- app/src/main/java/com/example/weatherapp/WeekWeather.java
+++ app/src/main/java/com/example/weatherapp/WeekWeather.java
@@ -3,6 +3,7 @@ package com.example.weatherapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,7 +15,13 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class WeekWeather extends AppCompatActivity {
     ImageView btnBack;
@@ -51,6 +58,13 @@ public class WeekWeather extends AppCompatActivity {
         }
 
         get7DaysData(city);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     public void get7DaysData(String data)
@@ -61,7 +75,47 @@ public class WeekWeather extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        try
+                        {
+                            JSONObject jsonObject = new JSONObject(response);
 
+                            JSONObject jsonObjectCity = jsonObject.getJSONObject("city");
+                            String city = jsonObjectCity.getString("name");
+                            txtName.setText(city);
+
+                            JSONArray jsonArrayList = jsonObject.getJSONArray("list");
+                            for (int i = 0; i < jsonArrayList.length(); i++)
+                            {
+                                JSONObject jsonObjectList = jsonArrayList.getJSONObject(i);
+
+                                String dt = jsonObjectList.getString("dt");
+                                long l = Long.valueOf(dt);
+                                Date date1 = new Date(l * 1000L);
+                                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                                String date = simpleDateFormat.format(date1);
+
+                                JSONObject jsonObjectTemp = jsonObjectList.getJSONObject("temp");
+                                String max = jsonObjectTemp.getString("max");
+                                String min = jsonObjectTemp.getString("min");
+                                Double max1 = Double.valueOf(max);
+                                Double min1 = Double.valueOf(min);
+                                String maxTemp = String.valueOf(max1);
+                                String minTemp = String.valueOf(min1);
+
+                                JSONArray jsonArrayWeather = jsonObjectList.getJSONArray("weather");
+                                JSONObject jsonObjectWeather = jsonArrayWeather.getJSONObject(0);
+                                String status = jsonObjectWeather.getString("description");
+                                String icon = jsonObjectWeather.getString("icon");
+
+                                arrayListWeather.add(new WeatherCustom(date, status, icon, maxTemp, minTemp));
+                            }
+
+                            customAdapter.notifyDataSetChanged();
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener() {
